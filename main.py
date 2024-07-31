bdcc03bc5f7a1b548fd59db7ca238c3b91fa98bc
Emilio Crocco <emilio.crocco@krminc.com>
2024-07-31 03:54:58 -0400

    Coding challenge * UI built and now returning links and videos * Updated init * WIP Dockerfile for IaC deploy

--- main.py
+++ main.py
@@ -18,6 +18,15 @@ def init():
   if "chat_history" not in st.session_state:
     st.session_state.chat_history = [AIMessage(content='Hello! Welcome to the chat! Do you have a question?')]
 
+  if 'messages' not in st.session_state:
+    st.session_state.messages = [
+      SystemMessage(content='You are a helpful assistant'),
+    ]
+
+  st.header('My Perplexity-like clone')
+  with st.sidebar:
+    st.link_button(label='Home', url='http://localhost:8501/', use_container_width=True)
+
 def get_response(user_query, chat_history):
   llm = ChatOllama(model = 'llama3.1')
 
@@ -38,6 +47,7 @@ def get_response(user_query, chat_history):
   links = soup.find_all('a')
 
   urls = []
+  videos = []
   for li in [link for link in links if link['href'].startswith('/url?q=')]:
     url = li['href']
     url = url.replace('/url?q=', '')
@@ -55,50 +65,46 @@ def get_response(user_query, chat_history):
       continue
     if '#' in url:
       url = url.split('#')[0]
+    if 'youtube.com/' in url or 'youtu.be/' in url:
+      videos.append(url)
     # malformed url, discard it
     if 'https://' not in url and 'http://' not in url:
      continue
     # add to urls list
     urls.append(url)
 
-
-  # dedupe urls after cleaning
+  # dedupe urls and videos after cleaning
   urls = list(np.unique(urls))[0:4]
+  videos = list(np.unique(videos))[0:4]
+
+  with st.container(height=300, border=True):
+    st.text('Here are some helpful links:')
+    for url in urls:
+      st.link_button(label=url, url=url, use_container_width=True)
+  with st.container(height=300, border=True):
+    for video in videos:
+      st.video(video)
 
   return chain.stream({
         'chat_history': chat_history,
         'user_question': user_query
       })
 
-def main():
-  init()
+# Init streamlit resources
+init()
 
-  if 'messages' not in st.session_state:
-    st.session_state.messages = [
-      SystemMessage(content='You are a helpful assistant'),
-    ]
+messages = st.session_state.get('chat_history', [])
+for i, msg in enumerate(messages):
+  if i % 2 == 0:
+    message(msg.content, is_user=True, key=str(i) + '_user')
+  else:
+    message(msg.content, is_user=False, key=str(i) + '_ai')
 
-  st.header('My Perplexity-like clone')
+user_input = st.chat_input('Your message: ', key='user_input')
 
-  messages = st.session_state.get('chat_history', [])
-  for i, msg in enumerate(messages):
-    if i % 2 == 0:
-      message(msg.content, is_user=True, key=str(i) + '_user')
-    else:
-      message(msg.content, is_user=False, key=str(i) + '_ai')
+if user_input:
+  st.session_state.chat_history.append(HumanMessage(content=user_input))
 
-  user_input = st.text_input('Your message: ', key='user_input')
-
-  with st.sidebar:
-    st.button('home')
-
-  if user_input:
-    st.session_state.chat_history.append(HumanMessage(content=user_input))
-
-    with st.spinner('Generating response...'):
-      response = st.write_stream(get_response(user_input, st.session_state.chat_history))
-    st.session_state.chat_history.append(AIMessage(content=response))
-
-
-if __name__ == '__main__':
-  main()
+  with st.spinner('Generating response...'):
+    response = st.write_stream(get_response(user_input, st.session_state.chat_history))
+  st.session_state.chat_history.append(AIMessage(content=response))
